d99951a7fa2b944d95256ea81a4b69cebb84c8b1
rexbu <rex@labjk.com>
2017-05-16 09:34:14 +0800

    makefile

--- src/base/Android.mk
+++ src/base/Android.mk
@@ -5,53 +5,12 @@ LOCAL_CFLAGS += -D__ANDROID__ -D__DEBUG__ -g
 #APP_PLATFORM := android-23
 
 LOCAL_C_INCLUDES	:=	\
-	$(LOCAL_PATH)/../../mc	\
-	$(LOCAL_PATH)/../../mc/basic	\
-	$(LOCAL_PATH)/../../mc/bs 		\
-	$(LOCAL_PATH)/../../mc/me		\
-	$(LOCAL_PATH)/../../third/sszip	\
-	$(LOCAL_PATH)/../../third/cjson	\
-	$(LOCAL_PATH)/../../third/oepnssl 			\
-	$(LOCAL_PATH)/../../third/openssl/include 	\
-	$(LOCAL_PATH)/../../third/curl				\
-	$(LOCAL_PATH)/../../third/curl/include 		\
-	$(LOCAL_PATH)/../../third/curl/include/curl \
 	$(LOCAL_PATH)/../base 			\
 	$(LOCAL_PATH)/../base/android 	\
-	$(LOCAL_PATH)/../filter 		\
-	$(LOCAL_PATH)/../core
+	$(LOCAL_PATH)/../filter 		
 
 SRCFILES = $(wildcard $(LOCAL_PATH)/*.cpp $(LOCAL_PATH)/*/*.cpp)
 SRCS = $(patsubst $(LOCAL_PATH)/%, ./%,$(SRCFILES)) 
-
-LOCAL_SRC_FILES += $(SRCS)
-# LOCAL_SRC_FILES += 		\
-# 	GPUBasic.cpp		\
-# 	GPUContext.cpp		\
-# 	GPUCropFilter.cpp	\
-# 	GPUDualport.cpp		\
-# 	GPUEglContext.cpp	\
-# 	GPUFilter.cpp		\
-# 	GPUFilterZ.cpp		\
-# 	GPUFilterBlend.cpp	\
-# 	GPUFilterBox.cpp	\
-# 	GPUFrameBuffer.cpp	\
-# 	GPUGroupFilter.cpp	\
-# 	GPUInput.cpp		\
-# 	GPUMutableInputFilter.cpp\
-# 	GPUOutput.cpp		\
-# 	GPUPixelBuffer.cpp	\
-# 	GPUProgram.cpp		\
-# 	GPURawInput.cpp		\
-# 	GPURawOutput.cpp	\
-# 	GPUTwoInputFilter.cpp	\
-# 	GPUTwoPassFilter.cpp	\
-# 	GPUView.cpp			\
-# 	GPUVertexBuffer.cpp	\
-# 	GPUYUV420Filter.cpp	\
-# 	GPUYUVFilter.cpp	\
-# 	GPUJointFilter.cpp	\
-# 	GPUZoomFilter.cpp	
 
 
 #LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog
--- src/filter/Android.mk
+++ src/filter/Android.mk
@@ -5,21 +5,9 @@ LOCAL_CFLAGS += -D__ANDROID__ -g
 #APP_PLATFORM := android-19
 
 LOCAL_C_INCLUDES	:=	\
-	$(LOCAL_PATH)/../../mc	\
-	$(LOCAL_PATH)/../../mc/basic	\
-	$(LOCAL_PATH)/../../mc/bs 		\
-	$(LOCAL_PATH)/../../mc/me		\
-	$(LOCAL_PATH)/../../third/sszip	\
-	$(LOCAL_PATH)/../../third/cjson	\
-	$(LOCAL_PATH)/../../third/oepnssl 			\
-	$(LOCAL_PATH)/../../third/openssl/include 	\
-	$(LOCAL_PATH)/../../third/curl				\
-	$(LOCAL_PATH)/../../third/curl/include 		\
-	$(LOCAL_PATH)/../../third/curl/include/curl \
 	$(LOCAL_PATH)/../base 			\
 	$(LOCAL_PATH)/../base/android 	\
-	$(LOCAL_PATH)/../filter 		\
-	$(LOCAL_PATH)/../core
+	$(LOCAL_PATH)/../filter
 
 SRCFILES = $(wildcard $(LOCAL_PATH)/*.cpp $(LOCAL_PATH)/*/*.cpp)
 SRCS = $(patsubst $(LOCAL_PATH)/%, ./%,$(SRCFILES)) 
