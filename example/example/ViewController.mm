42ec9cd4a38cd2dce2bff26d8ab0245693f1c40a
rexbu <rex@labjk.com>
2017-05-22 22:17:10 +0800

    贝塞尔曲线

--- example/example/ViewController.mm
+++ example/example/ViewController.mm
@@ -11,7 +11,7 @@
 #include "GPU.h"
 
 @interface ViewController (){
-    GPULineFilter* line;
+    GPUBezierFilter* line;
     GPUIOSView* view;
 }
 
@@ -24,36 +24,41 @@
     // Do any additional setup after loading the view, typically from a nib.
     bs_log_init("stdout");
     
-    line = new GPULineFilter();
+    line = new GPUBezierFilter();
     view = new GPUIOSView(self.view.bounds);
     [self.view addSubview:view->uiview()];
     
     line->addTarget(view);
-    gpu_point_t p[3];
+    
+    gpu_point_t p[5];
     p[0].x = 50;
     p[0].y = 80;
-    p[1].x = 600;
-    p[1].y = 700;
-    p[2].x = 20;
-    p[2].y = 1000;
-    line->setPoints(p, 3);
+    p[1].x = 400;
+    p[1].y = 500;
+    p[2].x = 100;
+    p[2].y = 800;
+    p[3].x = 300;
+    p[3].y = 1000;
+    p[4].x = 150;
+    p[4].y = 1150;
+    line->setPoints(p, 5);
     
     gpu_colorf_t c[3];
     c[0].r = 0;
     c[0].g = 0;
     c[0].b = 0;
     c[0].a = 1.0;
-    c[1].r = 0.4;
+    c[1].r = 0.7;
     c[1].g = 0.4;
-    c[1].b = 0.4;
-    c[1].a = 0.8;
+    c[1].b = 0.5;
+    c[1].a = 0.7;
     c[2].r = 0;
     c[2].g = 0;
     c[2].b = 0;
     c[2].a = 1.0;
-    line->setColors(c, 3);
     // 线条宽度
-    line->setLineWidth(30);
+    //line->setLineWidth(5);
+    line->setColors(c[0], c[1]);
     
     // 循环画
     [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(newFrame) userInfo:nil repeats:YES];
